c447612f17fa5e0109a7f340e729b237d9dcbb4c
mahdimangeli1381 <mahdimangeligl@gmail.com>
2024-09-03 16:18:26 +0330

    change in code server.js

--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+server.use(json());
 server.use(middlewares);
 server.use(router);
 module.exports = server;
